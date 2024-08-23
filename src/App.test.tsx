edf52c8d5982851deb9da0546f613d78ebc52400
dominicpam89 <dominicpam89@gmail.com>
2024-08-23 19:20:21 +0700

    success ininitialize app with shadcn

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import App from "./App";
-import { it, expect } from "vitest";
-import { screen, render } from "@testing-library/react";
-
-it("test basic component", () => {
-	render(<App />);
-	const logo = screen.getByLabelText("logo");
-	expect(logo).toBeVisible();
-});
