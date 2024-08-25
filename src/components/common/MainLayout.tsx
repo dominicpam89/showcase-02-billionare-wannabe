e1efb397242cf217c389153bae9f488f7f36aeff
dominicpam89 <dominicpam89@gmail.com>
2024-08-25 23:42:39 +0700

    update layout

--- src/components/common/MainLayout.tsx
+++ src/components/common/MainLayout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export default function MainLayout({ children }: PropsWithChildren) {
 	return (
-		<main className="w-full min-w-[320px] max-w-lg p-6 md:p-12 flex flex-col gap-8">
+		<main className="w-full min-w-[320px] max-w-lg p-6 md:p-12 flex flex-col gap-8 relative">
 			{children}
 		</main>
 	);
