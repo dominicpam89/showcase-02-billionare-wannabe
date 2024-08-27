95ecf54d306f969eec6fcb3208bae03bb29350b5
dominicpam89 <dominicpam89@gmail.com>
2024-08-27 22:04:30 +0700

    add single Coin item

--- src/components/main-app/trending-coins/Coins.tsx
+++ src/components/main-app/trending-coins/Coins.tsx
@@ -1,9 +1,19 @@
 import { ApiResponseTrendingCoins } from "@/lib/definition/trending-coins.type";
+import Coin from "./Coin";
 
 interface Props {
 	data: ApiResponseTrendingCoins["coins"];
 }
 export default function Coins({ data }: Props) {
 	console.log("from Coins component", data);
-	return <>Trending Coins</>;
+	return (
+		<div
+			aria-label="trending-coins-container"
+			className="flex flex-col gap-4 w-full"
+		>
+			{data.map((coin) => {
+				return <Coin data={coin} />;
+			})}
+		</div>
+	);
 }
